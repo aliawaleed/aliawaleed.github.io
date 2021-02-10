3b94263bc829aea012316e6dd449e14c6e4ea414
aliawaleed <57350259+aliawaleed@users.noreply.github.com>
2021-02-10 10:32:14 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 //mdn event reference
 
+//just for my own reference
 let myVariable;
 myVariable = "working";
 console.log(myVariable);
 
+// initialized as none so that they do not show initially
 aboutSection.style.display = "none"
 castSection.style.display = "none"
 
+// to show about paragraph only when about its clicked
 let aboutParagraph = document.getElementById('aboutID');
 aboutParagraph.addEventListener('click', () => {
   aboutSection.style.display = "block";
   castSection.style.display = "none";
 });
 
-let castParagraph = document.getElementById('castID');
-castParagraph.addEventListener('click', () => {
+// to show cast part only when about its clicked
+let castPart = document.getElementById('castID');
+castPart.addEventListener('click', () => {
   castSection.style.display = "block";
   aboutSection.style.display = "none";
 });
 
-let filmParagraph = document.getElementById('filmID');
-filmParagraph.addEventListener('click', () => {
+// to show movie only when its clicked
+let movie = document.getElementById('filmID');
+movie.addEventListener('click', () => {
   castSection.style.display = "none";
   aboutSection.style.display = "none";
 });
